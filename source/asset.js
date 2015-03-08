14253edc87a4bf33c4b5a97d27e3d3676e506315
Jack Tzu-Chieh Huang <tcjack@meepshop.com>
2015-03-08 11:27:49 +0800

    fix typo

--- source/asset.js
+++ source/asset.js
@@ -1,5 +1,5 @@
 export function fetch(load) {
-  let ver = System.meepworks && System.meepworks.version || '';
+  let ver = System.meepworks && System.meepworks.appVersion || '';
   return Promise.resolve(`load.address${ver}`);
 }
 export function instantiate(load) {
