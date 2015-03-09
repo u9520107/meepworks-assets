600b2ed3ef0090f9106abd20939df50d9fe99ecb
Jack Tzu-Chieh Huang <tcjack@meepshop.com>
2015-03-09 10:16:52 +0800

    revise path determination

--- source/asset.js
+++ source/asset.js
@@ -1,7 +1,12 @@
 export function fetch(load) {
   return Promise.resolve(load.address);
 }
+
+const httpReg = /https?:\/\/.*?\//i;
+
 export function instantiate(load) {
   let ver = System.meepworks && System.meepworks.appVersion || '';
-  return load.address + ver;
+  //remove hostname to align with server side generated paths
+  let address = load.address.replace(httpReg, '/');
+  return address + ver;
 }
